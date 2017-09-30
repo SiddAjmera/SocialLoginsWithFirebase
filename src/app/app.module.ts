a91cdda79357e42f80f71ebb0b22885656835277
Siddharth Ajmera <siddajmera@outlook.com>
2017-10-01 00:34:04 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FirebaseModule } from './firebase/firebase.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    FirebaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
